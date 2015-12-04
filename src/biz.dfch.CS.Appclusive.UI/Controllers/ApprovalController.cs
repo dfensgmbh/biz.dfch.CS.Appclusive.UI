5ff3b710e86ae79f86c5255afa2efa76089ac081
chreggenberger <eggenberger@afca.ch>
2015-12-04 11:13:03 +0100

    Corrections on Approval: * Status values always with capital first letter * only Approvals in State = 'Created' can be approved or declined * filter for approvals in list

--- src/biz.dfch.CS.Appclusive.UI/Controllers/ApprovalController.cs
+++ src/biz.dfch.CS.Appclusive.UI/Controllers/ApprovalController.cs
@@ -24,9 +24,26 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
         private biz.dfch.CS.Appclusive.Api.Core.Core coreRepository;
 
         // GET: Approvals
-        public ActionResult Index()
+        public ActionResult Index(int page = 1, string status = null)
         {
-            var items = CoreRepository.Approvals.Take(PortalConfig.Pagesize).ToList();
+            IEnumerable<Api.Core.Approval> items;
+            if (string.IsNullOrWhiteSpace(status))
+            {
+                int skipCount = page > 1 ? (page - 1) * PortalConfig.Pagesize : 0;
+                items = CoreRepository.Approvals.Skip(skipCount).Take(PortalConfig.Pagesize).ToList();
+                ViewBag.StatusFilter = status;
+            }
+            else
+            {
+                items = CoreRepository.Approvals.Where(a => a.Status == status);
+            }
+            switch (status)
+            {
+                case "Created": ViewBag.CreatedLinkClass = "active"; break;
+                case "Approved": ViewBag.ApprovedLinkClass = "active"; break;
+                case "Declined": ViewBag.DeclinedLinkClass = "active"; break;
+                default: ViewBag.AllLinkClass = "active"; break;
+            }
             return View(AutoMapper.Mapper.Map<List<Models.Core.Approval>>(items));
         }
 
@@ -44,7 +61,7 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
         {
             var apiItem = CoreRepository.Approvals.Where(c => c.Id == id).FirstOrDefault();
             Models.Core.Approval approval = AutoMapper.Mapper.Map<Models.Core.Approval>(apiItem);
-            approval.Status = Models.Core.Approval.APPROVED_STATUS;
+            approval.Status = Models.Core.Approval.APPROVED_STATUS_CHANGE;
             approval.HelpText = "The request will be approved when you click the 'Approve' button. You can optionally add a explanation or reason for approval.";
             return View("Edit", approval);
         }
@@ -54,7 +71,7 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
         {
             var apiItem = CoreRepository.Approvals.Where(c => c.Id == id).FirstOrDefault();
             Models.Core.Approval approval = AutoMapper.Mapper.Map<Models.Core.Approval>(apiItem);
-            approval.Status = Models.Core.Approval.DECLINED_STATUS;
+            approval.Status = Models.Core.Approval.DECLINED_STATUS_CHANGE;
             approval.HelpText = "The request will be declined when you click the 'Decline' button. You can optionally add a explanation or reason for approval.";
             return View("Edit", approval);
         }
