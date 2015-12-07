4ff6f8355dc8763e9483ceb1cbc90a8e83f08d8a
chreggenberger <eggenberger@afca.ch>
2015-12-07 17:09:08 +0100

    * Update cart -> not implemented n server side * update CartItem * DELETE Cart * DELETE CartItem
    close #13

--- src/biz.dfch.CS.Appclusive.UI/Controllers/CartsController.cs
+++ src/biz.dfch.CS.Appclusive.UI/Controllers/CartsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using biz.dfch.CS.Appclusive.UI.Models;
+using System.Diagnostics.Contracts;
 
 namespace biz.dfch.CS.Appclusive.UI.Controllers
 {
@@ -41,10 +42,67 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
             }
         }
 
+        // GET: Carts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var apiItem = CoreRepository.Carts.Expand("CartItems").Where(c => c.Id == id).FirstOrDefault();
+                return View(AutoMapper.Mapper.Map<Models.Core.Cart>(apiItem));
+            }
+            catch (Exception ex)
+            {
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
+                return View(new Models.Core.Cart());
+            }
+        }
+
+        // POST: Carts/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Models.Core.Cart cart)
+        {
+            try
+            {
+                var apiItem = CoreRepository.Carts.Expand("CartItems").Where(c => c.Id == id).FirstOrDefault();
+
+                #region copy all edited properties
+
+                apiItem.Name = cart.Name;
+                apiItem.Description = cart.Description;
+                
+                #endregion
+                CoreRepository.UpdateObject(apiItem);
+                CoreRepository.SaveChanges();
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).Add(new AjaxNotificationViewModel(ENotifyStyle.success, "Successfully saved"));
+                return View(AutoMapper.Mapper.Map<Models.Core.Cart>(apiItem));
+            }
+            catch (Exception ex)
+            {
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
+                if (null == cart.CartItems)
+                {
+                    cart.CartItems = new List<Models.Core.CartItem>();
+                }
+                return View(cart);
+            }
+        }
+
         // GET: Carts/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Api.Core.Cart apiItem = null;
+            try
+            {
+                apiItem = CoreRepository.Carts.Where(c => c.Id == id).FirstOrDefault();
+                CoreRepository.DeleteObject(apiItem);
+                CoreRepository.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
+                return View("Details", View(AutoMapper.Mapper.Map<Models.Core.Cart>(apiItem)));
+            }
         }
         
         public ActionResult CheckoutCart(int id)
@@ -77,8 +135,7 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
                 return View("Details", AutoMapper.Mapper.Map<Models.Core.Cart>(item));
             }
         }
-
-
+        
         #endregion
 
         #region edit CartItems
@@ -97,6 +154,73 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
             }
         }
 
+        // GET: Carts/Edit/5
+        public ActionResult ItemEdit(int id)
+        {
+            try
+            {
+                Contract.Requires(id > 0);
+                var apiItem = CoreRepository.CartItems.Expand("Cart").Expand("CatalogueItem").Where(c => c.Id == id).FirstOrDefault();
+                return View(AutoMapper.Mapper.Map<Models.Core.CartItem>(apiItem));
+            }
+            catch (Exception ex)
+            {
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
+                return View(new Models.Core.CartItem());
+            }
+        }
+
+        // POST: Carts/ItemEdit/5
+        [HttpPost]
+        public ActionResult ItemEdit(int id, Models.Core.CartItem cartItem)
+        {
+            try
+            {
+                Contract.Requires(id > 0);
+                Contract.Requires(null != cartItem);
+                var apiItem = CoreRepository.CartItems.Expand("Cart").Where(c => c.Id == id).FirstOrDefault();
+                Contract.Assert(null != apiItem);
+
+                #region copy all edited properties
+
+                apiItem.Name = cartItem.Name;
+                apiItem.Quantity = cartItem.Quantity;
+                apiItem.Description = cartItem.Description;
+                apiItem.Parameters = cartItem.Parameters;
+
+                #endregion
+                CoreRepository.UpdateObject(apiItem);
+                CoreRepository.SaveChanges();
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).Add(new AjaxNotificationViewModel(ENotifyStyle.success, "Successfully saved"));
+                return View(cartItem);
+            }
+            catch (Exception ex)
+            {
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
+                return View(cartItem);
+            }
+        }
+
+
+        // GET: Carts/ItemDelete/5
+        public ActionResult ItemDelete(int id)
+        {
+            Api.Core.CartItem apiItem = null;
+            try
+            {
+                Contract.Requires(id > 0);
+                apiItem = CoreRepository.CartItems.Where(c => c.Id == id).FirstOrDefault();
+                Contract.Assert(null != apiItem);
+                CoreRepository.DeleteObject(apiItem);
+                CoreRepository.SaveChanges();
+                return RedirectToAction("Details", new { Id = apiItem.CartId });
+            }
+            catch (Exception ex)
+            {
+                ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
+                return View("ItemDetails", View(AutoMapper.Mapper.Map<Models.Core.CartItem>(apiItem)));
+            }
+        }
         #endregion
     }
 }
