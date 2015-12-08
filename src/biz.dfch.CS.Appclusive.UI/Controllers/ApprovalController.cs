a4003e76b8ed2210e1f9bd94ae3df550832c9d10
chreggenberger <eggenberger@afca.ch>
2015-12-08 10:21:30 +0100

    * comment for Approval exception handling * return link from orders back to approval

--- src/biz.dfch.CS.Appclusive.UI/Controllers/ApprovalController.cs
+++ src/biz.dfch.CS.Appclusive.UI/Controllers/ApprovalController.cs
@@ -136,8 +136,9 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
             }
             catch (Exception ex)
             {
-                //TODO: ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
-                //return View("Edit", approval);
+                //TODO: once server api sends a re4adable response for approval updates use the outcommented lines instead
+                // ((List<AjaxNotificationViewModel>)ViewBag.Notifications).AddRange(ExceptionHelper.GetAjaxNotifications(ex));
+                // return View("Edit", approval);
                 ((List<AjaxNotificationViewModel>)ViewBag.Notifications).Add(new AjaxNotificationViewModel(ENotifyStyle.success, "Successfully " + approval.Status));
                 return View("Details", approval);
             }
