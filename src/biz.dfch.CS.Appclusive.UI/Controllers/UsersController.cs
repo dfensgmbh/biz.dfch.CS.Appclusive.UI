4c3756d040a3f73397fc3aac347cdaf8cfde67c2
Christian Eggenberger <eggenberger@afca.ch>
2015-12-11 14:19:01 +0100

    Screens for Users #30

--- src/biz.dfch.CS.Appclusive.UI/Controllers/UsersController.cs
+++ src/biz.dfch.CS.Appclusive.UI/Controllers/UsersController.cs
@@ -34,7 +34,7 @@ namespace biz.dfch.CS.Appclusive.UI.Controllers
         }
 
         // GET: Users/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(long id)
         {
             try
             {
