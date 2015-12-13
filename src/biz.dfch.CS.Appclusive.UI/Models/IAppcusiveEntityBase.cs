5d65c00369861ef7a88883bbe5486ed522ed409d
rufer7 <marc.rufer@d-fens.net>
2015-12-13 15:23:01 +0100

    * License headers added

--- src/biz.dfch.CS.Appclusive.UI/Models/IAppcusiveEntityBase.cs
+++ src/biz.dfch.CS.Appclusive.UI/Models/IAppcusiveEntityBase.cs
@@ -20,7 +20,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace biz.dfch.CS.Appclusive.UI.Models
 {
